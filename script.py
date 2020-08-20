d4ea080b87725d75775805e4e83feb3c610e1e49
James_Shields <jshi0044@student.monash.edu>
2020-08-20 17:22:18 +1000

    added code to generate playlist

--- script.py
+++ script.py
@@ -1,14 +1,22 @@
 import requests
 import json
 
-endpoint_url = 	'https://api.spotify.com/v1/recommendations'
-access_token = 'BQCsVf53VO7ZhxTEySC5IthvzDK0LAcvoZYZpV6rbzEHwMuQQ7TkxkmU5wX378QpsHKGbePTaWRcx0R4xzIDlT5TgvqpTmofP4_MrHDEJmUxqiUGZuqdHp_CFmiL7Y2JQltZ15TqVIzbkePG-DfrS-NJBvFgCl_HZP6DDREYfGdgs_OL'
+endpoint_url = "https://api.spotify.com/v1/recommendations?"
+access_token = "ENTER_ACCESS_TOKEN_HERE"
 
-#FILTERS
-limit = 10 #Number of songs in playlist
-market = 'AU' #Country
-seed_genres = "heavy_metal" #Genres 
-target_danceability = 0.2
-seed_artist = 'https://open.spotify.com/artist/3OpqU68JpZlzvjAJj3B2Da?si=5BpK2SFmQKGhxsdZR9nCHw'
+# OUR FILTERS
+limit: int = 10
+seed_genres: str = "pop"
+market: str = "AU"
+
+query: str = f"{endpoint_url}?limit={limit}&seed_genres={seed_genres}&market={market}"
+response = requests.get(query, headers={"Content-Type": "application/json", "Authorization":f"Bearer {access_token}"})
+
+uris: [] = []
+for (i,j) in enumerate(response.json()["tracks"]):
+    uris.append(j["uri"])
+    print(f"{i+1})  \"{j['name']}\" by {j['artists'][0]['name']}")
+
+
 
 
